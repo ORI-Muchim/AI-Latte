e1a447c243772786f7e5a858af44a4545d7b7375
ORI-Muchim <whals467746@gmail.com>
2024-01-07 23:45:14 +0900

    Edit get_model.py

--- get_model.py
+++ get_model.py
@@ -3,10 +3,11 @@ import os
 
 def download_file_if_not_exists(url, local_path):
     if os.path.exists(local_path):
-        return "모델 있음. 다운로드 건너뜀."
+        return "모델 파일이 있습니다."
 
     os.makedirs(os.path.dirname(local_path), exist_ok=True)
 
+    print("모델을 다운로드합니다. 잠시만 기다려 주세요.")
     response = requests.get(url)
     with open(local_path, 'wb') as file:
         file.write(response.content)
