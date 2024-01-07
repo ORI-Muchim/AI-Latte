a1062c9dc1cf2f06d3afb7d11f387c6c17393c3e
ORI-Muchim <whals467746@gmail.com>
2024-01-08 05:33:01 +0900

    Edit get_model.py

--- get_model.py
+++ get_model.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from tqdm import tqdm
 
 def download_model_if_not_exists(url, local_path):
     if os.path.exists(local_path):
@@ -8,8 +9,18 @@ def download_model_if_not_exists(url, local_path):
     os.makedirs(os.path.dirname(local_path), exist_ok=True)
 
     print("모델을 다운로드합니다. 잠시만 기다려 주세요.")
-    response = requests.get(url)
+    response = requests.get(url, stream=True)
+    
+    total_size_in_bytes = int(response.headers.get('content-length', 0))
+    block_size = 1024
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    
     with open(local_path, 'wb') as file:
-        file.write(response.content)
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+    progress_bar.close()
 
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        print("ERROR, something went wrong")
     return f"모델 저장됨: {local_path}"
